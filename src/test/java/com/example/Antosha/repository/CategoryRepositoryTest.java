1dde6e1f014f6f066bce1c38af7401640188a0d2
Kront2000 <ykront22@gmail.com>
2025-07-31 11:49:31 +0500

    Add database

--- src/test/java/com/example/Antosha/repository/CategoryRepositoryTest.java
+++ src/test/java/com/example/Antosha/repository/CategoryRepositoryTest.java
@@ -14,38 +14,5 @@ public class CategoryRepositoryTest {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    @Autowired
-    private DishRepository dishRepository;
-    @Autowired
-    private DishService dishService;
-
-    @Test
-    public void repositorySaveTest(){
-
-        Dish dish = Dish.builder()
-                .name("Шавуха")
-                .price(1200L)
-                .description("Ебать какая вкусная шувуха")
-                .image_path("/static/img/shavuha.png")
-                .category(categoryRepository.findByName("Суши").orElse(null))
-                .build();
-        System.out.println(dishRepository.save(dish));
-    }
-    @Test
-    public void repositoryFindAll(){
-        for(Dish dish: dishRepository.findAll()){
-            System.out.println(dish);
-        }
-    }
-
-    @Test
-    public void testSaveDish() {
-        System.out.println(dishRepository.findAll().get(0).getCategory());
-
-    }
-    @Test
-    public void testServiceDish(){
-        System.out.println(dishService.findAll());
-    }
 }
 
