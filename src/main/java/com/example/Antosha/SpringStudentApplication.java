c87832e24f50d3a3bbe1833c3b254e757181afb6
Kront2000 <ykront22@gmail.com>
2025-08-09 16:29:58 +0500

    use cloudinary

--- src/main/java/com/example/Antosha/SpringStudentApplication.java
+++ src/main/java/com/example/Antosha/SpringStudentApplication.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringStudentApplication {
 
 	public static void main(String[] args) {
+		// Загружаем .env
+		io.github.cdimascio.dotenv.Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv.load();
+
+		// Передаём Spring'у
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
 		SpringApplication.run(SpringStudentApplication.class, args);
 	}
