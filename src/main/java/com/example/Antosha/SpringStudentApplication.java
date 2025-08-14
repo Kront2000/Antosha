11a138698de876138f494e289f9cce0e5a9ccf31
Kront2000 <ykront22@gmail.com>
2025-08-14 15:48:44 +0500

    Finnish 1.0

--- src/main/java/com/example/Antosha/SpringStudentApplication.java
+++ src/main/java/com/example/Antosha/SpringStudentApplication.java
@@ -7,13 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringStudentApplication {
 
 	public static void main(String[] args) {
-		// Загружаем .env
-		io.github.cdimascio.dotenv.Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv.load();
 
-		// Передаём Spring'у
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
 		SpringApplication.run(SpringStudentApplication.class, args);
 	}
